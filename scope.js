a1d27d70bb7b799d9a997dea56df49222359c2bc
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-09-16 18:00:30 -1000

    Finished Scope

--- scope.js
+++ scope.js
@@ -2,14 +2,14 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen=false;
 }
 
 function openLid() {
-  /* answer here */
+  if (isCookieJarOpen === false){
+  return isCookieJarOpen = true;
+  }
 }
-
-
 // Fill in the return value for the inner function to return a string, 'Hello World' using only the variables provided
 // in both inner and outer functions
 function outerFunction() {
@@ -17,7 +17,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello +' '+ world; /* answer here */;
   }
   return innerFunction();
 }
@@ -31,8 +31,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -54,11 +54,11 @@ function sendDataToClient() {
       authenticated: false
     };
 
-    if (userObject.handle === username) {
-      userObject.authenticated = true;
-      return userObject
+    if (obj.handle === username) {
+      obj.authenticated = true;
+      return obj;
     } else {
-      return userObject
+      return obj;
     }
   }
   authenticateUser(userObject, 'neo')
